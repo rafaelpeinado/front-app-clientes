1863269ea052849cd438b437c963043ee6f8bcb3
rafaelpeinado <rafael.peinado.silva@gmail.com>
2021-03-26 22:40:48 -0300

    feat: configurando servidor

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-login',
@@ -12,9 +14,11 @@ export class LoginComponent {
   password: string;
   loginError: boolean;
   cadastrando: boolean;
+  mensagemSucesso: string;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
 
@@ -29,5 +33,19 @@ export class LoginComponent {
 
   cancelarCadastro(): void {
     this.cadastrando = false;
+  }
+
+  save() {
+    const usuario: Usuario = new Usuario();
+    usuario.username = this.username;
+    usuario.password = this.password;
+    this.authService.save(usuario)
+    .subscribe(response => {
+      this.mensagemSucesso = 'Cadastro realizado com sucesso" Efetue o login';
+      this.loginError = false;
+    }, error => {
+      this.mensagemSucesso = null;
+      this.loginError = true;
+    })
   }
 }
