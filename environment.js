3f2fad9810cb98594106a331bbf0a380e54e862b
rafaelpeinado <rafael.peinado.silva@gmail.com>
2021-03-26 23:12:57 -0300

    fix: arrumando variáveis de ambiente

--- environment.js
+++ environment.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const heroku = `export const environment = {
     production: true,
-    apiURLBase: '${process.env.CLIENTES-API}'
+    apiURLBase: '${process.env.CLIENTES_API}'
 };`
 
 fs.writeFile('src/environments/environment.prod.ts', heroku, (err, result) => {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiURLBase: 'undefined'
+    production: true,
+    apiURLBase: 'undefined'
 };
